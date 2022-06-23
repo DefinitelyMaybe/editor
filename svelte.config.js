c96e3d8a5a8c2e1aa6f717dffa75a43ec9771a35
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2022-06-23 13:35:47 +1200

    use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
-import adapter from 'svelte-adapter-deno';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-      out: 'build',
-      // deps: './deps.js' // (relative to adapter-deno package)
-		}),
+		adapter: adapter(),
 	},
 	preprocess: preprocess()
 };
